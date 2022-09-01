76a3426f42fb1beb9b7ce36b672418451de3a307
Narendra Maharjan <narendramj7@gmail.com>
2022-09-01 18:00:32 +0545

    completed

---
+++ voting.py
@@ -0,0 +1,14 @@
+user_name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+if age < 0:
+    print("Invalid age")
+
+if age > 18:
+    print("Vote submitted.")
+
+if age < 18:
+    print("Sorry, you need to be 18 or above to submite your vote.")
+
+if age == 18:
+    print("Your vote has been submitted.")
