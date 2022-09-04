b121ec71848ba651165b9d9cf057c273e489c842
Narendra Maharjan <narendramj7@gmail.com>
2022-09-04 07:09:00 +0545

    Completed

--- voting.py
+++ voting.py
@@ -4,11 +4,11 @@ age = int(input("Enter your age: "))
 if age < 0:
     print("Invalid age")
 
-if age > 18:
+elif age > 18:
     print(f"Dear {user_name}, your vote has been submitted.")
 
-if age < 18:
+elif age < 18:
     print(f"Dear {user_name}, sorry, you need to be 18 or above to submite your vote.")
 
-if age == 18:
+elif age == 18:
     print(f"Dear {user_name}, your vote has been submitted.")
